93184f88b857d7ff495218a6e1fbd22f06b93fef
Mihail Solovev <youoliver@mail.ru>
2025-07-05 14:21:43 +0300

    feat: generate short URL

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,14 @@
 package server
 
-import "context"
+import (
+	"context"
+	"github.com/p1xray/pxr-url-shortener/internal/dto"
+)
 
 // URLService represents a service for working with short URL.
 type URLService interface {
 	// Shorten generates a new unique short code for URL.
-	Shorten(ctx context.Context, longURL string) (string, error)
+	Shorten(ctx context.Context, longURL, host string) (dto.Shorten, error)
 
 	// LongURL returns a long URL by short code.
 	LongURL(ctx context.Context, shortCode string) (string, error)
